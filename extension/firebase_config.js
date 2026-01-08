823ea54dcb2ac580a57b7625c65bbb6410c06519
Lumos <contributor-4938cd5446@example.invalid>
2026-01-08 19:09:55 +0800

    Update Firebase Database URL to https://nomorescamtw-default-rtdb.firebaseio.com/

--- extension/firebase_config.js
+++ extension/firebase_config.js
@@ -1,4 +1,4 @@
 const FIREBASE_CONFIG = {
     // 您的 Firebase Realtime Database URL (例如: https://your-project-id.firebaseio.com)
-    databaseURL: "https://nomorescamtw.firebaseio.com",
+    databaseURL: "https://nomorescamtw-default-rtdb.firebaseio.com",
 };
